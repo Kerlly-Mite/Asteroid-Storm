121d2e31b011c84e8d01401e59e2194810ddc7d9
Kerlly Mite <belinda115uwu@gmail.com>
2026-06-23 11:52:11 -0500

    Agregar prueba de creación de asteroides

---
+++ test_model.py
@@ -0,0 +1,16 @@
+from model import create_initial_state
+
+
+state = create_initial_state(
+    10,
+    800,
+    600
+)
+
+
+print("Cantidad de asteroides:")
+print(len(state.asteroids))
+
+
+print("\nPrimer asteroide:")
+print(state.asteroids[0])
