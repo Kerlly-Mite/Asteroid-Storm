a2d762c864b4ebfe1d9f1ab49f1547e18b1e7299
Kerlly Mite <belinda115uwu@gmail.com>
2026-06-23 11:32:45 -0500

    Agregar modelo inicial de asteroides

---
+++ model.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from random import randint
+
+# ==========================
+# ENTIDAD ASTEROIDE
+# ==========================
+
+@dataclass(frozen=True)
+class Asteroid:
+    x: float
+    y: float
+    vx: float
+    vy: float
+    size: int
+
+# ==========================
+# ESTADO DEL JUEGO
+# ==========================
+
+@dataclass(frozen=True)
+class GameState:
+    asteroids: tuple
+
+# ==========================
+# CREAR ASTEROIDES
+# ==========================
+
+def create_asteroids(amount, width, height):
+
+    return tuple(
+        Asteroid(
+            x=randint(0, width),
+            y=randint(0, height),
+            vx=randint(-3, 3) or 1,
+            vy=randint(-3, 3) or 1,
+            size=randint(2, 5)
+        )
+        for _ in range(amount)
+    )
+
+# ==========================
+# CREAR ESTADO INICIAL
+# ==========================
+
+def create_initial_state(amount, width, height):
+
+    return GameState(
+        asteroids=create_asteroids(
+            amount,
+            width,
+            height
+        )
+    )
